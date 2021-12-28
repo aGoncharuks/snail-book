ebadc362ccd008c9d194b43570340cfea2e698d4
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-12-28 13:42:31 +0200

    feat(demo-widget): minor demo tweaks

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,12 +1,14 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import React from 'react';
 
 const Navigation = (): JSX.Element => {
   return (
     <nav>
-      {/*<Link href="/">*/}
-      {/*  <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>*/}
-      {/*</Link>*/}
+      <Link href="/">
+        <a className="text-gray-900 dark:text-white pr-6 py-4">
+          Вернуться на главную
+        </a>
+      </Link>
     </nav>
   );
 };
